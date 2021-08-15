ea017904d166c9ad45cec8675c985310323ddae0
Mr-Bowtie <mr.bowtie989@gmail.com>
2021-08-14 20:15:50 -0500

    created zipcode getter, legislators getter, and implemented custom letter generation

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,33 @@
 require 'csv'
-puts 'Event Manager Initialized.'
+require 'google/apis/civicinfo_v2'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody'],
+    )
+    legislators = legislators.officials
+
+    legislators_names = legislators.map(&:name)
+
+    legislators_string = legislators_names.join(', ')
+  rescue
+    'You can find your representative by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+template_letter = File.read('form_letter.html')
+
+puts 'Event Manager Initialized.'
 
 contents = CSV.open(
   'event_attendees.csv',
@@ -12,7 +36,13 @@ contents = CSV.open(
 )
 contents.each do |row|
   name = row[:first_name]
+
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
+  legislators = legislators_by_zipcode(zipcode)
+
+  personal_letter = template_letter.gsub('FIRST_NAME', name)
+  personal_letter.gsub!('LEGISLATORS', legislators)
+
+  puts personal_letter
 end
