8ef2cacbb9af79a5fdadf4014c8eae2b4721b558
Mr-Bowtie <mr.bowtie989@gmail.com>
2021-08-16 23:34:06 -0500

    implemented methods to identify peak registration hours and days

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -3,6 +3,8 @@ require 'time'
 require 'erb'
 require 'google/apis/civicinfo_v2'
 
+DAY_CONVERSION = { 0 => 'Monday', 1 => 'Tuesday', 2 => 'Wednesday', 3 => 'Thursday', 4 => 'Friday', 5 => 'Saturday', 6 => 'Sunday' }
+
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
@@ -27,7 +29,18 @@ def clean_phone_number(number)
   end
 end
 
-def peak_hours(__)
+# takes any array and returns a 3 element array containing the 3 most repeated elements in the given array
+def top_three(array)
+  el_count = {}
+  array.each do |h|
+    el_count.has_key?(h) ? el_count[h] += 1 : el_count[h] = 1
+  end
+  array.uniq.sort_by { |h| el_count[h] }.reverse.shift(3)
+end
+
+# takes an array of numbers generated from Date#wday and returns their equivalent as a string
+def day_converter(days)
+  days.map { |daynum| DAY_CONVERSION[daynum] }
 end
 
 def legislators_by_zipcode(zip)
@@ -65,16 +78,23 @@ contents = CSV.open(
   headers: true,
   header_converters: :symbol,
 )
+
+hours = []
+# an array of numbers (0-6) corresponding to days (monday is 0)
+days = []
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   number = clean_phone_number(row[:homephone])
   reg_date_time = Time.parse(clean_date_time(row[:regdate]))
-  #  legislators = legislators_by_zipcode(zipcode)
+  legislators = legislators_by_zipcode(zipcode)
+  form_letter = erb_template.result(binding)
+  save_thank_you_letter(id, form_letter)
 
-  #  form_letter = erb_template.result(binding)
-
-  #  save_thank_you_letter(id, form_letter)
-  puts "#{name} #{reg_date_time.hour}"
+  hours << reg_date_time.hour
+  days << reg_date_time.wday
 end
+puts "Personalized letters generated\n\n"
+puts "Peak registration hours are #{top_three(hours)}"
+puts "Most active registration days are #{day_converter(top_three(days))}"
